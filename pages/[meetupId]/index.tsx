fdb701be3b9caa3f6842f071442415be03483740
Oleksandr Poliukhovych <sonyks2007@ukr.net>
2022-07-29 02:41:06 +0300

    added getStaticPaths and getStaticProps

--- pages/[meetupId]/index.tsx
+++ pages/[meetupId]/index.tsx
@@ -1,17 +1,57 @@
-import type { NextPage } from "next";
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
 import { MeetupDetail } from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails: NextPage = () => {
+const MeetupDetails: NextPage = ({
+  meetupData,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <MeetupDetail
-        image="https://i.imgur.com/A040Lxr.png"
-        title="A First Meetup"
-        address="Some street"
-        description="The meetup description"
+        image={meetupData.image}
+        title={meetupData.title}
+        address={meetupData.address}
+        description={meetupData.description}
       />
     </>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const meetupId = context.params?.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image: "https://i.imgur.com/A040Lxr.png",
+        title: "A First Meetup",
+        address: "Some street",
+        description: "The meetup description",
+      },
+    },
+  };
 };
 
 export default MeetupDetails;
